92fbda287a07e6b9444110281bf7f74cfdae160f
eaminhu <eaminhu@163.com>
2016-08-04 16:16:22 +0800

    添加注释

--- js/app/api.js
+++ js/app/api.js
@@ -77,42 +77,42 @@ define([
 		},
 		
 		
-		/*树形结构*/
-			/* ele 为树形节点的ID
-			   type为类型，true为复选框 false 为普通树形
-			 */
-			ztree : function(ele,type){
-				var setting = {
-					check: {
-						enable: type
-					},
-					data: {
-						simpleData: {
-							enable: true
-						}
+		/*树形结构*
+		/* ele 为树形节点的ID
+		   type为类型，true为复选框 false 为普通树形
+		 */
+		ztree : function(ele,type){
+			var setting = {
+				check: {
+					enable: type
+				},
+				data: {
+					simpleData: {
+						enable: true
 					}
-				};
-				
-				var zNodes =[
-					{ id:1, pId:0, name:"随意勾选 1", open:true},
-					{ id:11, pId:1, name:"随意勾选 1-1", open:true},
-					{ id:111, pId:11, name:"随意勾选 1-1-1"},
-					{ id:112, pId:11, name:"随意勾选 1-1-2"},
-					{ id:12, pId:1, name:"随意勾选 1-2", open:true},
-					{ id:121, pId:12, name:"随意勾选 1-2-1"},
-					{ id:122, pId:12, name:"随意勾选 1-2-2"},
-					{ id:2, pId:0, name:"随意勾选 2", checked:true, open:true},
-					{ id:21, pId:2, name:"随意勾选 2-1"},
-					{ id:22, pId:2, name:"随意勾选 2-2", open:true},
-					{ id:221, pId:22, name:"随意勾选 2-2-1", checked:true},
-					{ id:222, pId:22, name:"随意勾选 2-2-2"},
-					{ id:23, pId:2, name:"随意勾选 2-3"}
-				];
-		
-				
-				$.fn.zTree.init($("#"+ele), setting, zNodes);
-				
-			}
+				}
+			};
+			
+			var zNodes =[
+				{ id:1, pId:0, name:"随意勾选 1", open:true},
+				{ id:11, pId:1, name:"随意勾选 1-1", open:true},
+				{ id:111, pId:11, name:"随意勾选 1-1-1"},
+				{ id:112, pId:11, name:"随意勾选 1-1-2"},
+				{ id:12, pId:1, name:"随意勾选 1-2", open:true},
+				{ id:121, pId:12, name:"随意勾选 1-2-1"},
+				{ id:122, pId:12, name:"随意勾选 1-2-2"},
+				{ id:2, pId:0, name:"随意勾选 2", checked:true, open:true},
+				{ id:21, pId:2, name:"随意勾选 2-1"},
+				{ id:22, pId:2, name:"随意勾选 2-2", open:true},
+				{ id:221, pId:22, name:"随意勾选 2-2-1", checked:true},
+				{ id:222, pId:22, name:"随意勾选 2-2-2"},
+				{ id:23, pId:2, name:"随意勾选 2-3"}
+			];
+	
+			
+			$.fn.zTree.init($("#"+ele), setting, zNodes);
+			
+		}
 		
 		
 	}
